ffee7a1e224d7c7b40cef961d90afc7e2dcd21b2
j4marcos <joaomarcos2004.jonh@gmail.com>
2023-12-21 15:30:16 -0300

    upload img background

--- src/scriptIndexPage.js
+++ src/scriptIndexPage.js
@@ -3,7 +3,7 @@ const gitSpan = document.body.querySelector(".nomePresentes")
 const partiesNames = ["amigo secreto","amigo oculto","amigo da onça","festas"]
 const gitsNames = ["presentes","surpresa", "tralhas", "lembrançinhas"]
 let idx = 0
-console.log(partySpan)
+let scrollAnimation = false
 
 setInterval(() => {
 
@@ -13,7 +13,3 @@ if (idx >= partiesNames.length -1) idx = 0
 else {idx++}
 
 }, 1000)
-
-window.addEventListener("scroll", ()=> {
-    window.scrollTo("1000px")
-})
