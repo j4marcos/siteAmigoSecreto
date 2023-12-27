194b95d998ac87da47acda6a313cd1a0408a47d9
j4marcos <joaomarcos2004.jonh@gmail.com>
2023-12-27 01:31:52 -0300

    feat: indexPage

--- src/scripts/scriptHomepage.js
+++ src/scripts/scriptHomepage.js
@@ -1,18 +1,40 @@
 const sessoes = document.querySelectorAll('section')
+const btns = document.querySelectorAll('button')
 const btnGrupos = document.querySelector('button.grupos')
 const btnAdicionar = document.querySelector('button.adicionar')
 const btnCriar = document.querySelector('button.criar')
 const btnPerfil = document.querySelector('button.perfil')
 
 btnGrupos.addEventListener('click', (e) => {
-    fecharSessoes()
-
+    fecharSessoes(e)
+    sessoes.item(0).classList.add('ativo')
+    carregarGrupos()
 })
 
-function fecharSessoes() {
+btnAdicionar.addEventListener('click', (e) => {
+    fecharSessoes(e)
+    sessoes.item(1).classList.add('ativo')
+})
+
+btnCriar.addEventListener('click', (e) => {
+    fecharSessoes(e)
+    sessoes.item(2).classList.add('ativo')
+})
+
+btnPerfil.addEventListener('click', (e) => {
+    fecharSessoes(e)
+    sessoes.item(3).classList.add('ativo')
+})
+
+function fecharSessoes(btn) {
+    btns.forEach((e)=> e.classList.remove('ativo'))
+    btn.srcElement.classList.add('ativo')
+
     sessoes.forEach((e)=> {
-        e.style.height = '0px'
-        console.log(e)
+        e.classList.remove('ativo')
     })
-    
+}
+
+function carregarGrupos() {
+    // metodo get
 }
--- src/scripts/scriptIndexPage.js
+++ src/scripts/scriptIndexPage.js
@@ -1,9 +1,40 @@
 const sessoes = document.querySelectorAll('section')
-const btnGrupos = document.querySelector('button.grupos')
-const btnAdicionar = document.querySelector('button.adicionar')
-const btnCriar = document.querySelector('button.criar')
+const btns = document.querySelectorAll('button')
+const btnInicio = document.querySelector('button.inicio')
+const btnEntrar = document.querySelector('button.entrar')
 const btnPerfil = document.querySelector('button.perfil')
 
-btnGrupos.addEventListener('click', (e) => {
+const festa = document.querySelector('.mudar')
+const nomesFestas = ['amigo secreto','amigo oculto','amigo chinelo', 'amigo doce', 'amigo da onça']
+let n = 0
 
+btnInicio.addEventListener('click', (e) => {
+    fecharSessoes(e)
+    sessoes.item(0).classList.add('ativo')
 })
+
+btnEntrar.addEventListener('click', (e) => {
+    fecharSessoes(e)
+    sessoes.item(1).classList.add('ativo')
+})
+
+btnPerfil.addEventListener('click', (e) => {
+    fecharSessoes(e)
+    sessoes.item(1).classList.add('ativo')
+})
+
+function fecharSessoes(btn) {
+    btns.forEach((e)=> e.classList.remove('ativo'))
+    btn.srcElement.classList.add('ativo')
+
+    sessoes.forEach((e)=> {
+        e.classList.remove('ativo')
+    })
+}
+
+
+setInterval(() => {
+    festa.innerText = nomesFestas[n]
+    n++
+    if (n >= nomesFestas.length) n =0
+}, 2000);
