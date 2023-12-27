e6d60c29b647fff76efc0b84c398ec4472099ad1
j4marcos <joaomarcos2004.jonh@gmail.com>
2023-12-27 18:51:13 -0300

    checkpoint

--- src/scripts/scriptHomepage.js
+++ src/scripts/scriptHomepage.js
@@ -4,6 +4,7 @@ const btnGrupos = document.querySelector('button.grupos')
 const btnAdicionar = document.querySelector('button.adicionar')
 const btnCriar = document.querySelector('button.criar')
 const btnPerfil = document.querySelector('button.perfil')
+const formCriarGrupo = document.querySelector('form.criarGrupo')
 
 btnGrupos.addEventListener('click', (e) => {
     fecharSessoes(e)
@@ -26,9 +27,15 @@ btnPerfil.addEventListener('click', (e) => {
     abrirSessao(sessoes.item(3))
 })
 
+formCriarGrupo.addEventListener('onchange', (e)=> {
+    e.preventDefault()
+    console.log("mudou")
+})
+
+
 function fecharSessoes(btn) {
     btns.forEach((e)=> e.classList.remove('ativo'))
-    btn.srcElement.classList.add('ativo')
+    if (btn) btn.srcElement.classList.add('ativo')
 
     sessoes.forEach((e)=> {
         e.classList.remove('ativo')
