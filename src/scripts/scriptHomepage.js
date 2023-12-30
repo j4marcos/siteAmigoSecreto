85220ec37fe5497367d9f8ffc76cba24e6d88417
j4marcos <joaomarcos2004.jonh@gmail.com>
2023-12-29 23:36:21 -0300

    fix : card info width

--- src/scripts/scriptHomepage.js
+++ src/scripts/scriptHomepage.js
@@ -9,20 +9,20 @@ let pageN = 0
 
 let data = localStorage.getItem('amigoSecretoData')
 if (data) {
-     data = JSON.parse(data)
-     mudarSessoes(data.paginaAtual)
+    data = JSON.parse(data)
+    mudarSessoes(data.paginaAtual)
 } else {
-    localStorage.setItem('amigoSecretoData',JSON.stringify({
-        paginaAtual : pageN,
+    localStorage.setItem('amigoSecretoData', JSON.stringify({
+        paginaAtual: pageN,
     }))
     atualizarLocalstorage()
 }
 
-function atualizarLocalstorage () {
+function atualizarLocalstorage() {
     data = (localStorage.getItem('amigoSecretoData'))
     data = JSON.parse(data)
     data.paginaAtual = pageN
-    
+
     localStorage.setItem('amigoSecretoData', JSON.stringify(data))
 }
 
@@ -40,13 +40,13 @@ btnGrupos.addEventListener('click', (e) => {
 btnAdicionarGrupo.forEach(e => {
     e.addEventListener('click', (e) => {
         mudarSessoes(2, e)
-})
+    })
 })
 
 btnCriarGrupo.forEach(e => {
     e.addEventListener('click', (e) => {
         mudarSessoes(3, e)
-})
+    })
 })
 
 
@@ -58,10 +58,10 @@ btnPerfil.addEventListener('click', (e) => {
 
 
 function mudarSessoes(n, btn) {
-    btns.forEach((e)=> e.classList.remove('ativo'))
+    btns.forEach((e) => e.classList.remove('ativo'))
     if (btn) btn.srcElement.classList.add('ativo')
 
-    sessoes.forEach((e)=> {
+    sessoes.forEach((e) => {
         e.classList.remove('ativo')
         e.classList.add("desativo")
     })
@@ -78,15 +78,15 @@ function carregarGrupos() {
 
 
 const nomesFesta = document.querySelectorAll('.mudar')
-const nomesFestas = ['amigo secreto','amigo oculto','amigo chinelo', 'amigo doce', 'amigo onça']
+const nomesFestas = ['amigo secreto', 'amigo oculto', 'amigo chinelo', 'amigo doce', 'amigo onça']
 let n = 0
 
 setInterval(() => {
-    nomesFesta.forEach((e)=>{
+    nomesFesta.forEach((e) => {
         e.innerText = nomesFestas[n]
     })
     n++
-    if (n >= nomesFestas.length) n =0
+    if (n >= nomesFestas.length) n = 0
 }, 2000);
 
 
@@ -107,16 +107,16 @@ const tipoFesta = cardPreview.querySelector(".tipoFesta")
 const regrasArea = cardPreview.querySelector('.regras')
 
 
-fortextarea.forEach((e)=> {
+fortextarea.forEach((e) => {
     e.addEventListener('input', (e) => {
-        let info = regrasArea.querySelector(`div.info.${"R"+regrasN}`)
+        let info = regrasArea.querySelector(`div.info.${"R" + regrasN}`)
         if (info) {
             info.innerText = e.target.value
             if (e.target.value === '') info.remove()
         } else {
             info = document.createElement('div')
             info.classList.add('info')
-            info.classList.add("R"+regrasN)
+            info.classList.add("R" + regrasN)
             regrasArea.appendChild(info)
             info.innerText = e.target.value
         }
@@ -131,17 +131,93 @@ btnAddRegra.addEventListener("click", (e) => {
     regraText.value = ''
 })
 
-grupoNome.addEventListener('input', (e) => {  
+grupoNome.addEventListener('input', (e) => {
     cardNome.innerText = grupoNome.value
 })
 
-checkListaDeDesejos.addEventListener('change', (e)=> {
+checkListaDeDesejos.addEventListener('change', (e) => {
     if (checkListaDeDesejos.checked) listaDeDesejos.classList.remove('desativo')
-    else  listaDeDesejos.classList.add('desativo') 
+    else listaDeDesejos.classList.add('desativo')
 })
 
-chechTipoFesta.addEventListener('change', (e)=> {
+chechTipoFesta.addEventListener('change', (e) => {
     tipoFesta.innerText = chechTipoFesta.value
 })
 
-form.addEventListener("submit", (e) => e.preventDefault())
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    console.log("tentando submit....")
+})
+
+
+
+
+
+// classe do canal tiger codes
+class FormSubmit {
+    constructor(settings) {
+    this.settings = settings
+    this.form = document.querySelector(settings.form)
+    this.formButtom = document.querySelector(settings.formButtom)
+    if (this.form) {
+        this.url = this.form.getAttribute("action")
+    }
+    this.sendForm = this.sendForm.bind(this)
+}
+
+displaySuccess() {
+    this.form.innerHTML = this.settings.success
+}
+
+
+getFormObject() {
+    const formObject = {}
+    const fields = this.form.querySelectorAll("[name]")
+    fields.forEach((field) => {
+        formObject[field.getAttribute("name")] = field.value
+    })
+    return formObject
+}
+
+
+onSubmission(event) {
+    event.preventDefault()
+    event.target.disabled = true
+    event.target.innerText = "enviando..."
+}
+
+async sendForm(event) {
+    this.onSubmission(event)
+    try {
+    await fetch(this.url, {
+        method: 'POST',
+        headers: {
+            "content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify(this.getFormObject()) ,
+    })
+    this.displaySuccess()
+    } catch (error) {
+        this.displayError()
+    }
+}
+
+displayError() {
+    this.form.innerHTML = this.settings.error
+}
+
+init() {
+    if(this.form) this.formButtom.addEventListener("click", this.sendForm)
+    return this
+}
+}
+
+const formSubmit = new FormSubmit({
+    form: "[data-form]",
+    button: "[data-buttom",
+    succces: "<h1 class='success'>Mensagem enviada!</h1>",
+    error: "<h1 class='error'>Não foi posivel enviar sua mensagem.</h1>"
+})
+
+formSubmit.init()
